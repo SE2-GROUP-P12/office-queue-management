84f56771fc68d15529a47d3862d5f57d483dc755
MattiaRiola <s280169@studenti.polito.it>
2021-10-12 20:59:14 +0200

    build: spring server application #1 #S2112OQM-15

---
+++ server/ezqueue/src/main/java/it/polito/ezqueue/EzqueueApplication.java
@@ -0,0 +1,13 @@
+package it.polito.ezqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzqueueApplication.class, args);
+	}
+
+}
