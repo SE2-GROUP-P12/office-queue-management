1c342a84fe553a1a2e44c26d1417450ce3bfbca5
Giuseppe Gagliard <giuseppe.gagliardi@studenti.polito.it>
2021-10-13 01:17:36 +0200

    build: built frontend backbone #1 #S2112OQM-14
    Built react frontend and addition of basic elements

---
+++ Front-end/queue-management/src/Actors.js
@@ -0,0 +1,24 @@
+import {Col} from 'react-bootstrap'
+function Customer() {
+    return (
+        <Col>
+            <h1>Customer</h1>
+        </Col>
+    );
+}
+function Employee() {
+    return (
+        <Col>
+            <h1>Employee</h1>
+        </Col>
+    );
+}
+function Manager() {
+    return (
+        <Col>
+            <h1>Manager</h1>
+        </Col>
+    );
+}
+
+export  {Customer, Employee, Manager} 
