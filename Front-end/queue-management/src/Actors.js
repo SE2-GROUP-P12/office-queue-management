527b6050e5439895dfef67456a0e0cacb14d12d5
STEFANO GRIVA s287729 <s287729@studenti.polito.it>
2021-10-13 18:24:24 +0200

    feat: added counter for cusomer and api call backbone

--- Front-end/queue-management/src/Actors.js
+++ Front-end/queue-management/src/Actors.js
@@ -1,23 +1,57 @@
 import {Col} from 'react-bootstrap'
 import {Button, Modal} from "react-bootstrap";
-import { useState} from "react";
+import { useEffect, useState} from "react";
 function Customer() {
     const [modalShow, setModalShow] = useState(false);
+    const [nextNumber, setNextNumber] = useState(0);//prossimo numero da dare
+    const buttonHandler= ()=>{
+        setNextNumber(nextNumber+1); 
+        setModalShow(true)
+        let tmp = {
+            "number": nextNumber,
+            "service": "placeholder"
+        }
+        /*
+        fetch('/customer/newNumber', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(tmp)
+        })*/ 
+    }
     return (
         <Col>
             <h1>Customer</h1>
-            <Button type="button" class="vertical-center" onClick={() => setModalShow(true)}>Get your number</Button>
-            <CustomerNumberModal show={modalShow} onHide={() => setModalShow(false)}/>
+            <Button type="button" class="vertical-center" onClick={() => {buttonHandler() }}>Get your number</Button>
+            <CustomerNumberModal show={modalShow} onHide={() => setModalShow(false)}number ={nextNumber}/>
         </Col>
     );
 }
 function Employee() {
+    const [nextNumber, setNextNumber] = useState(0);//prossimo numero da chiamare
     const [modalShow, setModalShow] = useState(false);
+    const [counterNumber, setCounterNumber] = useState(0)// numero del bancone
+    const buttonHandler = () => {
+        setModalShow(true);
+    /*scheletro per la chiamata a api 
+    useEffect(()=>{
+        fetch("/employee/getNext/"+counterNumber).then(response=>{
+            setNextNumber(response)
+        }) .catch(error => {
+            console.log(error);
+            updateSurveyList(()=>[]);
+            
+        })
+    
+    })*/
+    }
     return (
         <Col>
             <h1>Employee</h1>
-            <Button type="button" class="vertical-center" onClick={() => setModalShow(true)}>Call next customer</Button>
-            <EmployeeNumberModal show={modalShow} onHide={() => setModalShow(false)}/>
+            <Button type="button" class="vertical-center" onClick={() => buttonHandler()}>Call next customer</Button>
+            <EmployeeNumberModal show={modalShow} onHide={() => setModalShow(false)} number = {nextNumber}/>
         </Col>
     );
 }
@@ -43,7 +77,7 @@ function CustomerNumberModal(props){
             <Modal.Body>
                 <Col>
                 <div class= "centered-text" >Your number is </div>
-                <div class = "number-container" >1</div>
+                <div class = "number-container" >{props.number}</div>
                 </Col>
 
             </Modal.Body></Modal>
@@ -64,7 +98,7 @@ function EmployeeNumberModal(props){
             <Modal.Body>
                 <Col>
                 <div class= "centered-text" >The next customer number is </div>
-                <div class = "number-container" >1</div>
+                <div class = "number-container" >{props.number}</div>
                 </Col>
 
             </Modal.Body></Modal>
